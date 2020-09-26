d0c074506ea54ee4dfc835366921cfe0d7627204
ieseaacs <info@any1cansing.com>
2020-09-26 15:57:15 +0100

    happy with this bookstore today

---
+++ bookstore.js
@@ -0,0 +1,15 @@
+var book= [ 
+    {
+        name: '', 
+        author,
+        price, 
+        description, 
+        image
+    }
+    ];
+
+    function display() {
+        books.map((item)=> {
+            let titleElement = document.createElement('h1');
+        })
+    }
